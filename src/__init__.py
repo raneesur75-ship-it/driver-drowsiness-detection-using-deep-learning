4d2991c99861c57659aa2b281a187caf492ee6cc
raneesur75-ship-it <raneesur75@gmail.com>
2026-03-18 01:41:43 +0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Driver Drowsiness Detection System
+==================================
+Real-time drowsiness detection using CNN and Computer Vision.
+
+Author: Anees
+Version: 1.0.0
+"""
+
+__version__ = "1.0.0"
+__author__ = "Anees"
+
+# This file makes 'src' a Python package
+# It can be empty, but this info is helpful
